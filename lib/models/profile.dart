10209925677b0c1744c862db8adf26c0e0b60d36
asa90m <as.alahmer20@gmail.com>
2026-06-29 02:40:36 +0300

    Add Profile class with name, duration, and price

--- lib/models/profile.dart
+++ lib/models/profile.dart
@@ -1,1 +1,12 @@
 
+class Profile {
+  final String name;
+  final String duration;
+  final double price;
+
+  Profile({
+    required this.name,
+    required this.duration,
+    required this.price,
+  });
+}
