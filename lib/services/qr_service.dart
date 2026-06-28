d58f55d0a6001faf0b24e4616142ab77ecf60ad5
asa90m <as.alahmer20@gmail.com>
2026-06-29 02:21:49 +0300

    Add QrService class to generate QR codes

--- lib/services/qr_service.dart
+++ lib/services/qr_service.dart
@@ -1,1 +1,8 @@
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:flutter/material.dart';
 
+class QrService {
+  Widget generateQr(String data) {
+    return QrImageView(data: data, size: 200);
+  }
+}
