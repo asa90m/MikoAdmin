08e1d5fbc531d495cdcadd2dc3ba6ee7bcb73456
asa90m <as.alahmer20@gmail.com>
2026-06-29 02:39:53 +0300

    Add CardModel class with user details

--- lib/models/card.dart
+++ lib/models/card.dart
@@ -1,1 +1,14 @@
 
+class CardModel {
+  final String username;
+  final String password;
+  final String profile;
+  final String qrData;
+
+  CardModel({
+    required this.username,
+    required this.password,
+    required this.profile,
+    required this.qrData,
+  });
+}
