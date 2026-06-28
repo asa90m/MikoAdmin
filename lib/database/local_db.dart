895985cb66132adb1e39d62a88e1b3186ce53a94
asa90m <as.alahmer20@gmail.com>
2026-06-29 02:37:51 +0300

    Implement LocalDB class with database initialization

--- lib/database/local_db.dart
+++ lib/database/local_db.dart
@@ -1,1 +1,16 @@
 
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class LocalDB {
+  static Future<Database> init() async {
+    final dbPath = await getDatabasesPath();
+    return openDatabase(
+      join(dbPath, 'mikoadmin.db'),
+      onCreate: (db, version) {
+        db.execute('CREATE TABLE users(id INTEGER PRIMARY KEY, username TEXT, password TEXT, profile TEXT)');
+      },
+      version: 1,
+    );
+  }
+}
