e4364055d6df559db2fc457512f9abd64b5cdcd3
asa90m <as.alahmer20@gmail.com>
2026-06-29 02:21:16 +0300

    Add PrinterService class with printCard method
    Added a PrinterService class with a printCard method for printing cards.

--- lib/services/printer_service.dart
+++ lib/services/printer_service.dart
@@ -1,1 +1,6 @@
-
+class PrinterService {
+  Future<void> printCard(String username, String password) async {
+    // TODO: الطباعة عبر Bluetooth/WiFi/USB
+    print("Printing card for $username");
+  }
+}
