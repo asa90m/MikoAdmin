a8553ef0647104c3ec30d3cb93f703261d28b982
asa90m <as.alahmer20@gmail.com>
2026-06-29 02:16:21 +0300

    Add UsersScreen with basic layout and localization

--- lib/screens/users_screen.dart
+++ lib/screens/users_screen.dart
@@ -1,1 +1,14 @@
 
+import 'package:flutter/material.dart';
+
+class UsersScreen extends StatelessWidget {
+  const UsersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("إدارة المستخدمين")),
+      body: const Center(child: Text("قائمة المستخدمين")),
+    );
+  }
+}
