739be2ed6bc10f8b956ead282d76eb5f5ade4077
asa90m <as.alahmer20@gmail.com>
2026-06-29 02:14:46 +0300

    Refactor LoginScreen to StatelessWidget
    Refactor LoginScreen to use StatelessWidget and remove unnecessary state management.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,17 +1,7 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatefulWidget {
+class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
-
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  final ipController = TextEditingController();
-  final portController = TextEditingController(text: "8728");
-  final userController = TextEditingController();
-  final passController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -21,17 +11,12 @@ class _LoginScreenState extends State<LoginScreen> {
         padding: const EdgeInsets.all(16),
         child: Column(
           children: [
-            TextField(controller: ipController, decoration: const InputDecoration(labelText: "IP الراوتر")),
-            TextField(controller: portController, decoration: const InputDecoration(labelText: "Port")),
-            TextField(controller: userController, decoration: const InputDecoration(labelText: "اسم المستخدم")),
-            TextField(controller: passController, decoration: const InputDecoration(labelText: "كلمة المرور"), obscureText: true),
+            TextField(decoration: const InputDecoration(labelText: "IP الراوتر")),
+            TextField(decoration: const InputDecoration(labelText: "Port")),
+            TextField(decoration: const InputDecoration(labelText: "اسم المستخدم")),
+            TextField(decoration: const InputDecoration(labelText: "كلمة المرور"), obscureText: true),
             const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // TODO: إضافة خدمة الاتصال بالميكروتيك
-              },
-              child: const Text("اتصال"),
-            ),
+            ElevatedButton(onPressed: () {}, child: const Text("اتصال")),
           ],
         ),
       ),
