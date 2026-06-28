daa783386e8ad77a4cdf0855a9d3844e33c74873
asa90m <as.alahmer20@gmail.com>
2026-06-29 02:20:23 +0300

    Implement connect method in MikroTikService
    Added a connect method to establish a connection to the MikroTik API.

--- lib/services/mikrotik_service.dart
+++ lib/services/mikrotik_service.dart
@@ -1,1 +1,7 @@
 
+class MikroTikService {
+  Future<void> connect(String ip, int port, String user, String pass) async {
+    // TODO: إضافة كود الاتصال بالـ MikroTik API
+    print("Connecting to $ip:$port with user $user");
+  }
+}
