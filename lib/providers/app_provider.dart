b23788d37b3c0b2758ccd3a02da2f5822be7e7c2
asa90m <as.alahmer20@gmail.com>
2026-06-29 02:35:16 +0300

    Add AppProvider class for user management

--- lib/providers/app_provider.dart
+++ lib/providers/app_provider.dart
@@ -1,1 +1,13 @@
 
+import 'package:flutter/material.dart';
+
+class AppProvider extends ChangeNotifier {
+  String _currentUser = "";
+
+  String get currentUser => _currentUser;
+
+  void setUser(String user) {
+    _currentUser = user;
+    notifyListeners();
+  }
+}
