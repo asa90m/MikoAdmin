2d43b06e67b4f778fa0e1110971d6feb5b54e785
asa90m <as.alahmer20@gmail.com>
2026-06-29 02:41:32 +0300

    Add User model with username, password, and profile

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,1 +1,12 @@
 
+class User {
+  final String username;
+  final String password;
+  final String profile;
+
+  User({
+    required this.username,
+    required this.password,
+    required this.profile,
+  });
+}
