a1d6d43871ff02cdc927569da19c7411763d16e6
asa90m <as.alahmer20@gmail.com>
2026-06-29 02:34:13 +0300

    Create CardTemplate widget for user info display
    Added a CardTemplate widget to display user information.

--- lib/widgets/card_template.dart
+++ lib/widgets/card_template.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
 
+class CardTemplate extends StatelessWidget {
+  final String username;
+  final String password;
+  final String profile;
+
+  const CardTemplate({
+    super.key,
+    required this.username,
+    required this.password,
+    required this.profile,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text("User: $username"),
+        subtitle: Text("Pass: $password\nProfile: $profile"),
+      ),
+    );
+  }
+}
